622dad5e2f80d60c3476d0d816e4f1bfeb16fe61
ajisukmo44 <ajisuk31@gmail.com>
2020-02-10 15:41:36 +0700

    menambahakan file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Header, Navigation, Content} from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
@@ -8,22 +8,17 @@ function App() {
   return ( 
 <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="Aji Sukmo" scroll>
             <Navigation>
+                
+                <Link to="/">Home</Link>
+                <Link to="/project">Project</Link>
                 <Link to="/resume">Resume</Link>
                 <Link to="/about">About</Link>
-                <Link to="/project">project</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/about">About</Link>
-                <Link to="/project">project</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
+      
         <Content>
             <div className="page-content" />
             <Main/>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-
+import { Grid, Cell } from 'react-mdl';
+import foto from './profil.png';
 class LandingPage extends Component {
 render(){
 return(
+    <div style={{width:'100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+        <Cell col={12}>
+        <img src={foto} className="avatar-img" alt="logo"/>
+        <div  className="banner-text">
+        <h1>Freelance Web Developer</h1>
+        <hr/>
 
-<div> <h1> LandingPage </h1></div>
+        <p>HTML 5 | CSS 3 | Bootstrap 4 | NodeJs | PHP 7 | Mysql</p>
+        <div  className="social-links">
+            <a href="#" taget="_blank" rel="nooppener noreferrer">
+                <i className="fa fa-facebook-square" aria-hidden="true"/>
+            </a>
+            <a href="#" taget="_blank" rel="nooppener noreferrer">
+                <i className="fa fa-twitter-square" aria-hidden="true"/>
+            </a>
+            <a href="https://github.com/ajisukmo44" taget="_blank" rel="nooppener noreferrer">
+                <i className="fa fa-github-square" aria-hidden="true"/>
+            </a>
+            <a href="#" taget="_blank" rel="nooppener noreferrer">
+                <i className="fa fa-youtube-square" aria-hidden="true"/>
+            </a>
+        </div>
+        </div>
+      
+        </Cell>
+        </Grid>
+    
+    </div>
 
     )
 }
